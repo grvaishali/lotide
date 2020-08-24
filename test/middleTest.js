3d908f14ceba6b0b635d87f9b682ff012e549f7d
Vaishali Gupta <grvaishali@gmail.com>
2020-08-24 00:44:25 +0000

    Use moch for tail and middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,12 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([2, 3, 4, 5]), ([3, 4]));
-assertArraysEqual(middle([2, 3, 4, 5, 6]), ([4]));
+describe("#middle", () => {
+  it("returns [3, 4] for middle([2, 3, 4, 5])", () => {
+    assert.deepEqual(middle([2, 3, 4, 5]), [3, 4]);
+  });
+
+  it("returns [4] for middle(2, 3, 4, 5, 6])", () => {
+    assert.deepEqual(middle([2, 3, 4, 5, 6]), [4]);
+  });
+});
