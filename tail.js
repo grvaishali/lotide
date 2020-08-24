17c38b59b1d1589e3fe5fe106ec0be213112fee5
Vaishali Gupta <grvaishali@gmail.com>
2020-08-24 00:00:11 +0000

    Refactor tail

--- tail.js
+++ tail.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual}  !== ${expected}`);
-  }
-};
-
-const tail = function(arr) {
+const tail = function (arr) {
   if (arr.length > 1) {
     return arr.slice(1);
   } else {
@@ -14,7 +6,4 @@ const tail = function(arr) {
   }
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
+module.exports = tail;
